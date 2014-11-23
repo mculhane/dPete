a167afd53de653dd44c5b2e487bb86d139182a0b
Nick Carter <njc093@gmail.com>
2014-11-22 16:29:17 -0800

    switched from view to app level plugin, fixed bugs in syntax coloring, view loading, and made improvements to program loading logic

--- taskerator.py
+++ taskerator.py
@@ -6,22 +6,19 @@ class PeteControl(object):
     window = None
     tasks = None
     output = None
-    edit = None
 
 PETE = PeteControl()
 
+class PeteStartCommand(sublime_plugin.ApplicationCommand):
 
-class PeteStartCommand(sublime_plugin.TextCommand):
-
-    def run(self, edit):
+    def run(self):
         global PETE
 
-        PETE.edit = edit
-
+        """
+        Create and layout a new window
+        """
         sublime.run_command("new_window")
         PETE.window = sublime.active_window()
-
-        PETE.tasks = PETE.window.open_file("~/tasks.txt")
 
         PETE.window.set_layout({
             "cols": [0, 1],
@@ -29,17 +26,27 @@ class PeteStartCommand(sublime_plugin.TextCommand):
             "cells": [[0, 0, 1, 1], [0, 1, 1, 2]]
         })
 
+        """
+        Set up the output buffer
+        """
         PETE.output = PETE.window.new_file()
+        PETE.output.set_name("Next Tasks")
+        PETE.output.set_scratch(True)                   # Never report dirty for output window
+        PETE.output.assign_syntax('Packages/Pete.tmLanguage')
 
-        PETE.window.set_view_index(PETE.tasks, PETE.window.active_group(), 0)
-        PETE.window.set_view_index(PETE.output, PETE.window.active_group()+1, 0)
+        # For unclear reasons, the view index must be set before creating the next view. 
+        # Otherwise, this view is not displayed properly initially.
+        # There goes 3 hours of my life...
+        PETE.window.set_view_index(PETE.output, 1, 0)
+
+        """
+        Set up the tasks file buffer
+        """
+        PETE.tasks = PETE.window.open_file("~/tasks.txt")
+        self.setSyntax(3, PETE.tasks)                   # Wait for the view to load before apply settings
+        PETE.window.set_view_index(PETE.tasks, 0, 0)
+
         PETE.window.focus_view(PETE.tasks)
-
-        self.showPanel()
-
-    def addTask(self, s):
-        global PETE
-        PETE.tasks.run_command("pete_insert", {"task": s})
         self.showPanel()
 
     def showPanel(self):
@@ -48,16 +55,22 @@ class PeteStartCommand(sublime_plugin.TextCommand):
             "Add task:", "",
             self.addTask, None, self.showPanel)
 
+    def setSyntax(self, trys, view):
+        if (view.is_loading()):
+            sublime.set_timeout(lambda: self.setSyntax(trys-1, view), 500)
+        else:
+            view.assign_syntax('Packages/Pete.tmLanguage')
 
-class PeteInsertCommand(sublime_plugin.TextCommand):
-
-    def run(self, edit, task=""):
-        self.view.insert(edit, self.view.size(), task + "\n")
+    def addTask(self, s):
+        global PETE
+        PETE.tasks.run_command("append", {"characters": s + "\n"})
+        PETE.tasks.run_command("save")
+        self.showPanel()
 
 
 class PeteProcessTasks(sublime_plugin.EventListener):
 
-    def on_post_save(self, view):
+    def on_post_save_async(self, view):
         global PETE
 
         if (view == PETE.tasks):
@@ -66,6 +79,4 @@ class PeteProcessTasks(sublime_plugin.EventListener):
             lines = view.lines(region)
 
             for l in lines:
-                PETE.output.run_command(
-                    "pete_insert",
-                    {"task": view.substr(l)})
+                PETE.output.run_command("append", {"characters": view.substr(l) + "\n"})
