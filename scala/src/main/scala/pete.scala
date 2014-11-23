f793d6083ca0c5da889cca257eb6598c039c9572
Nick Carter <njc093@gmail.com>
2014-11-23 13:16:38 -0800

    created starter main object in pete

--- scala/src/main/scala/pete.scala
+++ scala/src/main/scala/pete.scala
@@ -1,3 +1,18 @@
-object pete extends App {
+package pete
 
+import scala.io.Source
+import parser.PeteParser
+
+object pete extends App {
+	var lines = new collection.mutable.MutableList[String]()
+	for (ln <- io.Source.stdin.getLines) {
+	  lines += ln
+	}
+	
+	val result = PeteParser(lines.foldLeft("")((a:String, b:String) => a + b))
+	val output = semantics.semantics.compute_next_due_tasks(result.get, 10)
+	
+	for (ln <- output) {
+	  println(ln)
+	}
 }
