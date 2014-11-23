e4036c1c6e94afc51c70b218e0110141e9f89e2e
Nick Carter <njc093@gmail.com>
2014-11-22 16:38:46 -0800

    factored large function into multiple smaller functions

--- taskerator.py
+++ taskerator.py
@@ -14,9 +14,15 @@ class PeteStartCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         global PETE
 
+        self.create_layout()
+
+
+    def create_layout(self):
         """
         Create and layout a new window
         """
+        global PETE
+
         sublime.run_command("new_window")
         PETE.window = sublime.active_window()
 
@@ -26,9 +32,18 @@ class PeteStartCommand(sublime_plugin.ApplicationCommand):
             "cells": [[0, 0, 1, 1], [0, 1, 1, 2]]
         })
 
+        self.setup_output_buffer()
+        self.setup_tasks_buffer()
+
+        PETE.window.focus_view(PETE.tasks)
+        self.showPanel()
+
+    def setup_output_buffer(self):
         """
         Set up the output buffer
         """
+        global PETE
+
         PETE.output = PETE.window.new_file()
         PETE.output.set_name("Next Tasks")
         PETE.output.set_scratch(True)                   # Never report dirty for output window
@@ -39,15 +54,15 @@ class PeteStartCommand(sublime_plugin.ApplicationCommand):
         # There goes 3 hours of my life...
         PETE.window.set_view_index(PETE.output, 1, 0)
 
+    def setup_tasks_buffer(self):
         """
         Set up the tasks file buffer
         """
+        global PETE
+        
         PETE.tasks = PETE.window.open_file("~/tasks.txt")
         self.setSyntax(3, PETE.tasks)                   # Wait for the view to load before apply settings
         PETE.window.set_view_index(PETE.tasks, 0, 0)
-
-        PETE.window.focus_view(PETE.tasks)
-        self.showPanel()
 
     def showPanel(self):
         global PETE
