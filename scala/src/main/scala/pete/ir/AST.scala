6a0b6227ffcb0bdae4bd3e34e9b0c9f5ea5f3d23
Michael Culhane <mculhane11@gmail.com>
2014-12-03 14:11:36 -0800

    Tweaked IR to store dependence at the task level

--- scala/src/main/scala/pete/ir/AST.scala
+++ scala/src/main/scala/pete/ir/AST.scala
@@ -9,14 +9,13 @@ sealed abstract class TimeLiteral extends Expr
 case class TaskList(tasks: Seq[Task]) extends AST
 
 case class Task(start: Option[Expr], due: Option[Expr], done: Option[Expr],
-				recurrence: Option[Recurrence], description: String,
-				hash: String) extends AST
+				dependence: Option[String], recurrence: Option[Recurrence],
+				description: String, hash: String) extends AST
 
 case class Before(expr: Expr, offset: Offset) extends Expr
 case class After(expr: Expr, offset: Offset) extends Expr
-case class Start(hash: String) extends Expr
-case class Due(hash: String) extends Expr
-case class Done(hash: String) extends Expr
+case object Start extends Expr
+case object Due extends Expr
 
 case class TimeStamp(datetime: DateTime) extends TimeLiteral
 case class Every(offset: Offset) extends TimeLiteral
