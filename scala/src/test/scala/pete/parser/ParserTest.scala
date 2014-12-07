3a8f89481201251104b761eccab263843e05c59b
Michael Culhane <mculhane11@gmail.com>
2014-12-07 01:47:38 -0800

    Tweaked tasks to accommodate new IR

--- scala/src/test/scala/pete/parser/ParserTest.scala
+++ scala/src/test/scala/pete/parser/ParserTest.scala
@@ -8,7 +8,6 @@ import pete.ir.Expr
 import pete.ir.Task
 import pete.ir.TaskList
 import pete.ir.TimeStamp
-import pete.ir.Every
 import pete.ir.Offset
 import pete.ir.Recurrence
 
@@ -29,7 +28,7 @@ class TaskParserTests extends FunSpec with LangParseMatchers[Task] {
   
   describe("A valid task") {
     it("works in the general case") {
-      program("#ABCDEF Take out the trash @ 2012-10-06 - 2013-10-04 % every 2 weeks") should parseAs(Task(Some(TimeStamp(DateTime.parse("2012-10-06"))), Some(TimeStamp(DateTime.parse("2013-10-04"))), None, Some(Recurrence(Every(Offset(2, "week")))), "Take out the trash", "#ABCDEF"))
+      program("#ABCDEF Take out the trash @ 2012-10-06 - 2013-10-04 % every 2 weeks") should parseAs(Task(Some(TimeStamp(DateTime.parse("2012-10-06"))), Some(TimeStamp(DateTime.parse("2013-10-04"))), None, Some(Recurrence(Offset(2, "week"))), "Take out the trash", "#ABCDEF"))
     }
   }  
 }
